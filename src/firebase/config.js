634213f9700e0cac7a09aa8aee7a6390889df076
JudgeFudge19 <dremukare@gmail.com>
2021-10-28 22:11:13 +0300

    install react-dotenv and created script to create .env file on netlify deploy

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase/app';
+import env from 'react-dotenv';
 import 'firebase/auth';
 import 'firebase/firestore';
 
 const app = firebase.initializeApp({
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	apiKey: env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: env.REACT_APP_FIREBASE_APP_ID,
 });
 
 const projectFirestore = firebase.firestore();
